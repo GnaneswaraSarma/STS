3b8865bc75d944d243e04f61a8fcba1a0031807c
GnaneswaraSarma <45159842+GnaneswaraSarma@users.noreply.github.com>
2019-02-13 19:29:50 +0530

    Add files via upload

---
+++ MyFirstRestApp/src/main/java/com/rest/app/MyFirstRestAppApplication.java
@@ -0,0 +1,14 @@
+package com.rest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstRestAppApplication.class, args);
+	}
+
+}
+
